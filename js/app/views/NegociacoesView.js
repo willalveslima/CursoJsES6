0c038e98b3c51976bc0825b81e02331870f7f252
Willian Alves Lima <w.alves.lima@gmail.com>
2020-05-15 01:45:35 +0000

    Aula 05 acerto em view

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -17,17 +17,15 @@ class NegociacoesView {
                         </tr>
                     </thead>
                     <tbody>
-                    ${model.negociacoes.map(n => {
+                    ${model.negociacoes.map(n => `
+                            <tr>
+                                <td>${DateHelper.dataParaTexto(n.data)}</td>
+                                <td>${n.quantidade}</td>
+                                <td>${n.valor}</td>
+                                <td>${n.volume}</td>
+                            </tr>
 
-                        return `
-                          <tr>
-                              <td>${DateHelper.dataParaTexto(n.data)}</td>
-                              <td>${n.quantidade}</td>
-                              <td>${n.valor}</td>
-                              <td>${n.volume}</td>
-                          </tr>
-                        `
-                        })}
+                        `).join('')}
                     </tbody>
                     <tfoot>
                         <td colspan="3"></td>
