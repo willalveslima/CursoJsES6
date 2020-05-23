f7fb5cc4224f2b3d3e51d73f15a55d34efeaccde
Willian Alves Lima <w.alves.lima@gmail.com>
2020-05-23 23:26:48 +0000

    Aula 3-03 DAO

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -10,16 +10,35 @@ class NegociacaoController {
         this._mensagem = new Bind(new Mensagem(),
             new MensagemView($("#mensagemView")), 'texto');
         this._ordemAtual = '';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                this._mensagem.texto = erro;});
     }
     adiciona(event) {
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._limparCampos();
-        this._mensagem.texto = "Negociação Incluída com sucesso";
-    }
-   ordena(coluna) {
+        ConnectionFactory
+            .getConnection()
+            .then(conexao => {
+                let negociacao = this._criaNegociacao();
+                new NegociacaoDao(conexao)
+                    .adiciona(negociacao)
+                    .then(() => {
+                        this._listaNegociacoes.adiciona(negociacao);
+                        this._mensagem.texto = 'Negociação adicionada com sucesso';
+                        this._limparCampos();   
+                    });
+        })
+        .catch(erro => this._mensagem.texto = erro);
+    }    
+    ordena(coluna) {
         if(this._ordemAtual == coluna) {
-            this._listaNegociacoes.inverteOrdem();
+          this._listaNegociacoes.inverteOrdem();
         } else {
             this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
         }
@@ -27,8 +46,14 @@ class NegociacaoController {
     }
     apaga(event) {
         event.preventDefault();
-        this._listaNegociacoes.apaga();
-        this._mensagem.texto = "Lista de negociações apagadas com com sucesso";
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.apaga();
+            });
     }
     importaNegociacoes(event) {
         event.preventDefault();
@@ -58,8 +83,8 @@ class NegociacaoController {
     _criaNegociacao() {
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
     }
     _limparCampos() {
         this._inputData.value = '';
