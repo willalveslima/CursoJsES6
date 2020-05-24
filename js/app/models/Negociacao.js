fb9e299e9924de6a0577616cd1e5594d98747de6
Willian Alves Lima <w.alves.lima@gmail.com>
2020-05-24 19:24:31 +0000

    Aula 3-04 isEquals

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -18,4 +18,7 @@ class Negociacao {
     get volume(){
         return this._quantidade * this._valor;
     }
+    isEquals(outraNegociacao) {        
+        return JSON.stringify(this) == JSON.stringify(outraNegociacao)
+    }
 }
